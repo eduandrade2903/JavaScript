d3229e63177faf6451d8823d2f3649e65573dc8f
Eduardo <eduandrade.lara@hotmail.com>
2023-09-18 18:37:46 -0300

    exercicios

---
+++ exercicios/imc.js
@@ -0,0 +1,39 @@
+/* calcular o imc dado 
+
+1,75
+90kg
+*/
+
+/*let altura = 1.75;
+let massa = 90;
+
+let calcula = massa / ( 1.75 * 1.75);
+
+console.log(`O imc calculado é ${calcula}`);*/
+/************************************************ */
+
+/*for (let i = 0; i <= 10 ; i++) {
+    for(let j = 0; j <= 10; j++) {
+        console.log(`${j} X ${i} = `,[i]*[j]);
+    }
+    
+}*/
+
+/******************************************** */
+
+const palavra = 'abacate'.toLowerCase();
+let letras = {}
+
+for (let i = 0; i < palavra.length; i++) { 
+
+    if(letras[palavra[i]] = 1) {
+        letras[palavra[i]] = 1++;
+    }else {
+        letras[palavra[i]] = 1;
+    }
+
+    letras[palavra[i]] = 1;
+    console.log([letras]);
+}
+
+
