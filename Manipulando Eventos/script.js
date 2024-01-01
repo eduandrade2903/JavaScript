d57ee816d36ed71405f9be02c338e4908a0d124d
Eduardo <eduandrade.lara@hotmail.com>
2024-01-01 15:54:55 -0300

    finalizando manipulação de eventos

--- Manipulando Eventos/script.js
+++ Manipulando Eventos/script.js
@@ -45,25 +45,25 @@
 /*delegação de eventos */
 //Para evitar repetição de código
 /*Validando eventos */
-const menu = document.querySelector("#menu");
+// const menu = document.querySelector("#menu");
 
-menu?.addEventListener("click", (event) => {
-    const {target} = event;
+// menu?.addEventListener("click", (event) => {
+//     const {target} = event;
     
-    switch(  target.getAttribute("class")) {
-        case "home":
-            console.log("home");
-        break;
+//     switch(  target.getAttribute("class")) {
+//         case "home":
+//             console.log("home");
+//         break;
 
-        case "sobre":
-            console.log("sobre");
-        break;
+//         case "sobre":
+//             console.log("sobre");
+//         break;
 
-        case "contato":
-            console.log("contato");
-        break;
+//         case "contato":
+//             console.log("contato");
+//         break;
 
-    }
-});
+//     }
+// });
 
 
